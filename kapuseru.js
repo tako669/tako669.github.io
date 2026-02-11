632846e91e537b22add0a5235228498e112d4d89
tako669 <takuyaisitakugood@gmail.com>
2026-02-11 16:02:47 +0900

    Add files via upload

---
+++ kapuseru.js
@@ -0,0 +1,44 @@
+const gachaList = [
+  "一期一会",
+  "異口同音",
+  "温故知新",
+  "自業自得",
+  "十人十色",
+  "以心伝心",
+  "電光石火",
+  "一石二鳥",
+  "百発百中",
+  "前代未聞",
+  "完全無欠",
+  "試行錯誤",
+  "優柔不断",
+  "四面楚歌",
+  "起死回生",
+  "臥薪嘗胆",
+  "森羅万象",
+  "大器晩成",
+  "天真爛漫",
+];
+
+function drawGacha() {
+  const randomIndex = Math.floor(Math.random() * gachaList.length);
+  const result = gachaList[randomIndex];
+
+  const video = document.getElementById("gachaVideo");
+  const resultText = document.getElementById("result");
+
+    
+
+  resultText.textContent = "";       // いったん消す
+  video.style.display = "block";     // 動画表示
+  video.play();                      // 再生
+
+  setTimeout(() => {
+    video.style.display = "none";    // 動画消す
+    resultText.textContent = result; // 結果表示
+  }, 8000);  // ← 動画の秒数に合わせる
+}
+
+const button = document.getElementById("gachaButton");
+button.addEventListener("click", drawGacha);
+
